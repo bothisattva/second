0ebca9a59b00f8986fe887884903306ce3f915ab
bothisattva <orlov21423@mail.ru>
2019-01-13 15:02:42 +0300

    Задаk целочисленный массив, состоящий из элементов 0 и 1. . С помощью цикла и условия заменил 0 на 1, 1 на 0;

--- src/second.java
+++ src/second.java
@@ -1,19 +1,34 @@
 public class second {
 
     public static void main (String[] args) {
-        //№2
+
+        
+        //№3
+        int [] mas3 = {1,1,0,0,0,1,1,0,1};
+        for (int i: mas3 ){
+            if (i == 0){
+                i=1;
+            }
+            else {
+                i = 0;
+            }
+        }
+
+        //№1
+        /*
         int [] mas2 = {1,5,3,2,11,4,5,2,4,8,9,1};
         for (int i : mas2) {
             if (i < 6) {
                 i *= 2;
             }
-        }
-    }
-        //№1
+        }*/
+
+        //№2
         /*
         int [] mas1 = new int [8];
         for (int i = 0; i <mas1.length; i++) {
             mas1[i]= i * 3;
         }*/
+    }
     }
 

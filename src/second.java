5e398661e59b1463eb54bcece4d4434af8941a78
bothisattva <orlov21423@mail.ru>
2019-01-13 18:01:41 +0300

    5 нахождение максимального элемента массива

--- src/second.java
+++ src/second.java
@@ -2,8 +2,32 @@ public class second {
 
     public static void main (String[] args) {
 
-        
+        //№5
+        int []mas5 = {1,2,3,4,5,6,};
+        int max=mas5[0];
+        for (int i :mas5){
+            if (i>max){
+                max=i;
+            }
+        }
+        System.out.println(max);
+/*
+        //№4 не доделано
+        int [][] mas4 = new int [4][4];
+        for (int i = 0; i <mas4.length; i++) {
+            mas4[i][i] = 1;
+            int j = mas4[i].length - i ;
+            mas4[i][j] = 1;
+        for (int i = 0; i <mas4.length ; i++) {
+            for (int j = 0; j < mas4[i].length; j++) {
+                System.out.print(mas4[i][j]);
+            }
+            System.out.println(" ");
+        }
+*/
+
         //№3
+        /*
         int [] mas3 = {1,1,0,0,0,1,1,0,1};
         for (int i: mas3 ){
             if (i == 0){
@@ -12,7 +36,7 @@ public class second {
             else {
                 i = 0;
             }
-        }
+        }*/
 
         //№1
         /*

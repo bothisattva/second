972d2b22c43c339eaf4c2ec590fa2dcc41fdca9a
bothisattva <orlov21423@mail.ru>
2019-01-13 14:47:29 +0300

    создал пустой массив размеров 8.  С помощью цикла заполнил его значениями 0 3 6 9 12 15 18 21;

---
+++ src/second.java
@@ -0,0 +1,9 @@
+public class second {
+    //№1
+    public static void main(String[] args) {
+        int [] mas1 = new int [8];
+        for (int i = 0; i <mas1.length; i++) {
+            mas1[i]= i * 3;
+        }
+    }
+}

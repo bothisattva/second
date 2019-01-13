dee5435d38a802d74089fa2b269bd9e3553907e6
bothisattva <orlov21423@mail.ru>
2019-01-13 14:56:30 +0300

    Создал заполненный массив чисел, числа которые меньше 6 удвоил

--- src/second.java
+++ src/second.java
@@ -1,9 +1,19 @@
 public class second {
-    //№1
-    public static void main(String[] args) {
+
+    public static void main (String[] args) {
+        //№2
+        int [] mas2 = {1,5,3,2,11,4,5,2,4,8,9,1};
+        for (int i : mas2) {
+            if (i < 6) {
+                i *= 2;
+            }
+        }
+    }
+        //№1
+        /*
         int [] mas1 = new int [8];
         for (int i = 0; i <mas1.length; i++) {
             mas1[i]= i * 3;
-        }
+        }*/
     }
-}
+

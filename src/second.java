2a24dd12aaf52361636ec0aad3dbadfc399ade61
bothisattva <orlov21423@mail.ru>
2019-01-16 19:07:32 +0300

    задание 6

--- src/second.java
+++ src/second.java
@@ -1,32 +1,46 @@
 public class second {
 
-    public static void main (String[] args) {
 
-        //№5
-        int []mas5 = {1,2,3,4,5,6,};
-        int max=mas5[0];
-        for (int i :mas5){
-            if (i>max){
-                max=i;
-            }
+
+        public static void main(String[] args) {
+            //№5
+        /*
+        int[] mas5 = {1, 2, 3, 4, 5, 6,};
+        int max = mas5[0];
+        int min = mas5[0];
+        for (int i : mas5) {
+           if  (i > max){
+                    max = i;
         }
-        System.out.println(max);
-/*
-        //№4 не доделано
-        int [][] mas4 = new int [4][4];
-        for (int i = 0; i <mas4.length; i++) {
+            if (i<min){
+                min=i;
+            }
+    }
+    */
+
+
+            //№4
+        /*
+        int[][] mas4 = new int[4][4];
+        int  j;
+
+            for (int i = 0; i <mas4.length; i++) {
             mas4[i][i] = 1;
-            int j = mas4[i].length - i ;
-            mas4[i][j] = 1;
-        for (int i = 0; i <mas4.length ; i++) {
-            for (int j = 0; j < mas4[i].length; j++) {
-                System.out.print(mas4[i][j]);
+            j = mas4[i].length - i ;
+            mas4[i][j] = 1; // вот эта строка ?
+    }
+
+        for (int i = 0; i < mas4.length; i++) {
+            for (int l = 0; l < mas4.length; l++) {
+                System.out.print(mas4[i][l]);
+
             }
             System.out.println(" ");
+
         }
 */
 
-        //№3
+            //№3
         /*
         int [] mas3 = {1,1,0,0,0,1,1,0,1};
         for (int i: mas3 ){
@@ -38,7 +52,7 @@ public class second {
             }
         }*/
 
-        //№1
+            //№1
         /*
         int [] mas2 = {1,5,3,2,11,4,5,2,4,8,9,1};
         for (int i : mas2) {
@@ -47,12 +61,29 @@ public class second {
             }
         }*/
 
-        //№2
+            //№2
         /*
         int [] mas1 = new int [8];
         for (int i = 0; i <mas1.length; i++) {
             mas1[i]= i * 3;
         }*/
-    }
-    }
+
+
+
+
+        }
+//№6
+  private boolean check1 ( int[] mass6){
+            int middle = mass6.length % 2;
+            int sumleft = 0, sumright = 0;
+            middle = (middle > 0) ? (middle /= 2) : ((middle / 2) + 1);
+                for (int i = 0; i < mass6.length; i++) {
+                    middle = (i < middle) ? (sumleft += mass6[i]) : (sumright += mass6[i]);
+                }
+            boolean ch = (sumleft == sumright) ? true : false;
+            return ch;
+                }
+
+}
+
 
